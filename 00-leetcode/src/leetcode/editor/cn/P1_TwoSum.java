8ba99f789bab42165a754cb0f2b7e4a5e784dae6
codetheory <89645874@qq.com>
2024-02-21 11:37:39 +0800

    [LeetCode] 01 两数之和 双指针

---
+++ 00-leetcode/src/leetcode/editor/cn/P1_TwoSum.java
@@ -0,0 +1,105 @@
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 只会存在一个有效答案 
+// 
+//
+// 
+//
+// 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？ 
+//
+// Related Topics 数组 哈希表 👍 18297 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+/**
+ * 两数之和
+ * @author CodeTheory
+ * @date 2024-02-21 11:03:33
+ */
+public class P1_TwoSum{
+	 public static void main(String[] args) {
+	 	 Solution solution = new P1_TwoSum().new Solution();
+		 for (int i : solution.twoSum(new int[]{2, 7, 11, 15}, 9)) {
+			 System.out.println(i);
+		 }
+		 for (int i : solution.twoSum(new int[]{3, 2, 4}, 6)) {
+			 System.out.println(i);
+		 }
+		 for (int i : solution.twoSum(new int[]{3, 3}, 6)) {
+			 System.out.println(i);
+		 }
+
+	 }
+	 
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+		int[] tempArray = new int[nums.length];
+		System.arraycopy(nums, 0, tempArray, 0, nums.length);
+		Arrays.sort(nums);
+		int[] result = new int[2];
+		int left = 0, right = nums.length - 1;
+		while (left < right) {
+			int temp = nums[left] + nums[right];
+			if ( temp < target) {
+				left++;
+			} else if (temp > target) {
+				right--;
+			} else {
+				break;
+			}
+		}
+		boolean flag = true;
+		for (int i = 0;  i < tempArray.length; i++) {
+			if (tempArray[i] == nums[left] && flag) {
+				result[0] = i;
+				flag = false;
+			}if (tempArray[i] == nums[right]) {
+				result[1] = i;
+			}
+		}
+		return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
