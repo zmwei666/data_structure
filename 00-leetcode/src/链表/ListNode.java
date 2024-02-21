e11acd38e2431ebf68d5b5171c0cfad263b8991c
codetheory <89645874@qq.com>
2024-02-21 18:43:16 +0800

    [LeetCode] 203 移除链表元素 虚拟头节点

--- 00-leetcode/src/链表/ListNode.java
+++ 00-leetcode/src/链表/ListNode.java
@@ -1,9 +1,28 @@
 package 链表;
 
 public class ListNode {
-	int val;
-	ListNode next;
-	ListNode(int x) { 
-		val = x; 
+	public int val;
+	public ListNode next;
+
+	public ListNode(){}
+	public ListNode(int val) {
+		this.val = val;
+	}
+	public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+	public static ListNode arrayToListNode(int[] arr) {
+		ListNode head = new ListNode(arr[0]);
+		ListNode prev = head;
+		for (int i = 1; i < arr.length; i++) {
+			ListNode listNode = new ListNode(arr[i]);
+			prev.next = listNode;
+			prev = listNode;
+		}
+		return head;
+	}
+
+	@Override
+	public String toString() {
+		return  val +"-" + next;
 	}
 }
