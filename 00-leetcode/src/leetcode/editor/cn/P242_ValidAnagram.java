74c09a904dd0a0691ee6f82c11112ec5affde935
codetheory <89645874@qq.com>
2024-02-23 11:35:18 +0800

    [LeetCode] 242 有效的字母异位词 哈希表优化

--- 00-leetcode/src/leetcode/editor/cn/P242_ValidAnagram.java
+++ 00-leetcode/src/leetcode/editor/cn/P242_ValidAnagram.java
@@ -35,8 +35,6 @@
 
 package leetcode.editor.cn;
 
-import java.util.HashMap;
-
 /**
  * 有效的字母异位词
  * @author CodeTheory
@@ -53,29 +51,21 @@ public class P242_ValidAnagram{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isAnagram(String s, String t) {
-		if (s.length() != t.length()) {
-			return false;
-		}
-		HashMap<Character, Integer> mapS = new HashMap<>();
+		int[] record = new int[26];
 		for (int i = 0; i < s.length(); i++) {
-			if (mapS.containsKey(s.charAt(i))) {
-				mapS.put(s.charAt(i), mapS.get(s.charAt(i)) + 1);
-			} else {
-				mapS.put(s.charAt(i), 1);
-			}
+			record[s.charAt(i) - 'a']++;
 		}
 
 		for (int i = 0; i < t.length(); i++) {
-			if (mapS.containsKey(t.charAt(i)) && mapS.get(t.charAt(i)) != 0) {
-				mapS.put(t.charAt(i), mapS.get(t.charAt(i)) - 1);
-			} else {
+			record[t.charAt(i) - 'a']--;
+		}
+
+		for (int i : record) {
+			if (i != 0) {
 				return false;
 			}
 		}
-
 		return true;
-
-
 	}
 }
 //leetcode submit region end(Prohibit modification and deletion)
