2573bdb34b2436c14a9df47fed23fbcc6e7de2f5
codetheory <89645874@qq.com>
2024-02-26 10:24:47 +0800

    [LeetCode] 349 两个数组的交集 哈希数组

--- 00-leetcode/src/leetcode/editor/cn/P349_IntersectionOfTwoArrays.java
+++ 00-leetcode/src/leetcode/editor/cn/P349_IntersectionOfTwoArrays.java
@@ -31,7 +31,8 @@
 
 package leetcode.editor.cn;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 两个数组的交集
@@ -57,21 +58,26 @@ public class P349_IntersectionOfTwoArrays{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-		HashSet<Integer> set = new HashSet<>();
-		int size = 0;
-		for (int i = 0; i < nums1.length; i++) {
-			set.add(nums1[i]);
+		int[] hash1 = new int[1002];
+		int[] hash2 = new int[1002];
+		for(int i : nums1) {
+			hash1[i]++;
 		}
-		for (int i = 0; i < nums2.length; i++) {
-			if (set.contains(nums2[i])) {
-				nums1[size] = nums2[i];
-				size++;
-				set.remove(nums2[i]);
+		for(int i : nums2) {
+			hash2[i]++;
+		}
+		List<Integer> resList = new ArrayList<>();
+		for(int i = 0; i < 1002; i++) {
+			if(hash1[i] > 0 && hash2[i] > 0) {
+				resList.add(i);
 			}
 		}
-		int[] result = new int[size];
-		System.arraycopy(nums1,0,result,0, size);
-		return result;
+		int index = 0;
+		int res[] = new int[resList.size()];
+		for(int i : resList) {
+			res[index++] = i;
+		}
+		return res;
 	}
 }
 //leetcode submit region end(Prohibit modification and deletion)
